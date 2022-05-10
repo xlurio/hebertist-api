d9ddb78c5264bba7608ef04845fc14d32c37550f
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-05-09 23:42:14 -0300

    user model and user manager created

---
+++ api/core/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, username, email, password, **kwargs):
+        """Creates and returns a new user"""
+        if not username:
+            raise ValueError('An username must be set')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **kwargs
+        )
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password, **kwargs):
+        """Creates and returns a common user"""
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **kwargs)
+
+    def create_superuser(self, username, email, password, **kwargs):
+        """Creates and returns a common user"""
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+
+        if not kwargs.get("is_staff"):
+            raise ValueError('Superuser must have is_staff=True')
+        if not kwargs.get("is_superuser"):
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(username, email, password, **kwargs)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model"""
+    username = models.CharField(max_length=24, unique=True)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateField(default=timezone.now)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager
+
+    @property
+    def is_staff(self):
+        """Returns if the user is staff"""
+        return self.is_staff
+
+    class Meta:
+        # Visible name of the model
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
---
+++ api/core/tests/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class ModelsTests(TestCase):
+    """Models tests"""
+
+    def test_create_user(self):
+        """Testing valid user creation"""
+        pass
